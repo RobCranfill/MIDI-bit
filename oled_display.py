969556cdb01c0ae40b85a7d7a595467805ed1c1a
Rob Cranfill <robcranfill@gmail.com>
2024-10-19 09:43:35 -0700

    multi line display ok

--- oled_display.py
+++ oled_display.py
@@ -22,35 +22,64 @@ class oled_display:
         display_bus = I2CDisplayBus(i2c, device_address=0x3C)
 
         WIDTH = 128
-        HEIGHT = 32  # Change to 64 if needed
+        HEIGHT = 32
 
         display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=WIDTH, height=HEIGHT)
 
         # Make the display context
-        splash = displayio.Group()
-        display.root_group = splash
+        root = displayio.Group()
+        display.root_group = root
 
         color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
         color_palette = displayio.Palette(1)
         color_palette[0] = 0x000000  # black
         bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
-        splash.append(bg_sprite)
+        root.append(bg_sprite)
 
-        # Create the label
-        text = "Hello World!"
-        self.text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=28, y=HEIGHT // 2 - 1)
-        splash.append(self.text_area)
+        # Create the labels
+        LINE_HEIGHT = 10
+        x = 0
+        y = 5
+        self.text_area_1 = label.Label(terminalio.FONT, color=0xFFFFFF, x=x, y=y)
+        root.append(self.text_area_1)
 
-    def set_text(self, text):
-        self.text_area._set_text(text, 1.0)
+        y += LINE_HEIGHT
+        self.text_area_2 = label.Label(terminalio.FONT, color=0xFFFFFF, x=x, y=y)
+        root.append(self.text_area_2)
+
+        y += LINE_HEIGHT
+        self.text_area_3 = label.Label(terminalio.FONT, color=0xFFFFFF, x=x, y=y)
+        root.append(self.text_area_3)
+
+        # y += LINE_HEIGHT
+        # self.text_area_4 = label.Label(terminalio.FONT, color=0xFFFFFF, x=x, y=y)
+        # root.append(self.text_area_4)
+
+
+    def set_text_1(self, text):
+        self.text_area_1._set_text(text, 1.0)
+
+    def set_text_2(self, text):
+        self.text_area_2._set_text(text, 1.0)
+
+    def set_text_3(self, text):
+        self.text_area_3._set_text(text, 1.0)
+
+    # def set_text_4(self, text):
+    #     self.text_area_4._set_text(text, 1.0)
+
 
     def test(self):
 
-        self.set_text("One!")
+        self.set_text_1("This is a test.")
+        self.set_text_2("This is only a test.")
+        self.set_text_3("1234567890123456789012345")
+
         time.sleep(1)
-        self.set_text("Two!")
+        self.set_text_1("This was a test.")
+    
         time.sleep(1)
-        self.set_text("Three!")
+        self.set_text_1("This will be a test.")
 
         print("test done")
         # while True:
