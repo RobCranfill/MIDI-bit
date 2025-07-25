890cf6eef8dfd024b840dc99656d4eb7fade36dc
robcranfill <robcranfill@gmail.com>
2025-07-24 19:45:06 -0700

    MSM start time

--- midi_state_machine.py
+++ midi_state_machine.py
@@ -5,6 +5,9 @@ call 'note()' method with each new note;
 if the sequence is completed, note() will return true.
 '''
 
+import time
+
+
 class midi_state_machine:
 
     def __init__(self, note_list, debug=False):
@@ -17,44 +20,51 @@ class midi_state_machine:
         self.debug_ = debug
 
         # this is the index of the last in-sequence item we got; -1 if none
-        self.last_hit_ = -1
+        self._last_hit = -1
+        self._first_hit_time = 0 # right?
 
         self.debug_print(f"MSM: {note_list=}")
 
 
-    def debug_print(self, str):
+    def debug_print(self, msg):
         if self.debug_:
-            print(str)
+            print(msg)
 
     def note(self, new_note):
         """Return true iff this new note completes the sequence."""
 
-        self.debug_print(f"Testing {new_note=}; {self.last_hit_=}")
+        self.debug_print(f"Testing {new_note=}; {self._last_hit=}")
 
-        if new_note != self.note_list_[self.last_hit_ + 1]:
-            self.debug_print("  Next is NOT in sequence")
+        if new_note != self.note_list_[self._last_hit + 1]:
 
-            # But what if it's FIRST in the sequence?
+            # First in the sequence? (FIXME: Is this check out of order?)
             if new_note == self.note_list_[0]:
-                self.debug_print("  But it IS first in sequence")
-                self.last_hit_ = 0
+                self.debug_print("  Note is first in sequence")
+                self._last_hit = 0
+                self._first_hit_time = time.monotonic()
                 return False
 
-            self.last_hit_ = -1
+            self.debug_print("  Next is NOT in sequence")
+            # not in sequence. reset.
+            self._last_hit = -1
+            self._first_hit_time = 0 # right?
             return False
 
-        self.last_hit_ += 1
-        self.debug_print(f"  Note is a hit! Now at index {self.last_hit_} of target")
+        self._last_hit += 1
+        self.debug_print(f"  Note is a hit! Now at index {self._last_hit} of target")
 
-        if self.last_hit_ < len(self.note_list_) - 1:
+        if self._last_hit < len(self.note_list_) - 1:
             self.debug_print(f"  Not at end of list yet ")
             return False
 
         self.debug_print(" ^^^ Complete!")
-        self.last_hit_ = -1
+        self._last_hit = -1
         return True
 
+    def get_seq_start_time(self):
+        return self._first_hit_time
 
+    
 def test():
 
     msm = midi_state_machine((3, 4, 5))
